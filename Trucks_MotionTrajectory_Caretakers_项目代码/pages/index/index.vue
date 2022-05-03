be7a8de663350812ea32511c85f10094a34bff7a
冷瞳9I6 <lt9i6@qq.com>
2022-05-03 23:51:49 +0800

    调试

--- Trucks_MotionTrajectory_Caretakers_项目代码/pages/index/index.vue
+++ Trucks_MotionTrajectory_Caretakers_项目代码/pages/index/index.vue
@@ -3,68 +3,79 @@
 		<view class="VievButton" id="mainMenu">
 			<img class='VievButtonImg' src="/static/Vector.png" alt="" srcset="">
 		</view>
-		<view class="radiusVievButton" id="myPosition">
+		<view class="radiusVievButton" id="myPosition" @click="">
 			<img class='VievButtonImg' src="@/static/Crosshair.png" alt="" srcset="">
 		</view>
-		
 		<map class="map"></map>
 	</view>
 </template>
 
 <script>
-	
+	export default {
+		name: 'TrucksMap',
+		data() {
+			return {
+
+			}
+		},
+	}
 </script>
 
 <style>
+	.content {
+		width: 750rpx;
+		height: auto;
+	}
 
-.content{
-	width: 750rpx;
-	height: auto;
-}
-.map{
-	width: 750rpx;
-	height: 100vh;
-}
-.VievButton{
-	width: 80rpx;
-	height: 80rpx;
-	background-color: white;
-	border-radius: 5rpx;
-	border: 0.5px inside #e4e4e4;
-	box-shadow: 0px 1px 6px rgba(216, 216, 216, 0.5);
-	display: grid; //生成3*3网格布局
-	grid-template-columns: 20% 60% 20%;
-	grid-template-rows: 20% 60% 20%;
-}
-.VievButtonImg{
-	width: 100%;
-	height: 100%;
-	grid-row-start: 2; // 上边框是第二根网格线
-	grid-column-start: 2; // 左边框是第二根网格线
-	grid-row-end: 3; // 下边框是第三根网格线
-	grid-column-end: 3; // 右边框是第三根网格线
-}
-.radiusVievButton{
-	width: 80rpx;
-	height: 80rpx;
-	background-color: white;
-	border-radius: 50%;
-	border: 0.5px inside #e4e4e4;
-	box-shadow: 0px 1px 6px rgba(216, 216, 216, 50%);
-	display: grid; //生成3*3网格布局
-	grid-template-columns: 10% 80% 10%;
-	grid-template-rows: 10% 80% 10%;
-}
-#myPosition{
-	z-index: 1;
-	position: fixed;
-	top: 75vh;
-	right: 5vw;	
-}
-#mainMenu{
-	z-index: 1;
-	position: fixed;
-	top: 75vh;
-	left: 5vw;	
-}
+	.map {
+		width: 750rpx;
+		height: 100vh;
+	}
+
+	.VievButton {
+		width: 80rpx;
+		height: 80rpx;
+		background-color: white;
+		border-radius: 5rpx;
+		border: 0.5px inside #e4e4e4;
+		box-shadow: 0px 1px 6px rgba(216, 216, 216, 0.5);
+		display: grid; //生成3*3网格布局
+		grid-template-columns: 20% 60% 20%;
+		grid-template-rows: 20% 60% 20%;
+	}
+
+	.VievButtonImg {
+		width: 100%;
+		height: 100%;
+		grid-row-start: 2; // 上边框是第二根网格线
+		grid-column-start: 2; // 左边框是第二根网格线
+		grid-row-end: 3; // 下边框是第三根网格线
+		grid-column-end: 3; // 右边框是第三根网格线
+	}
+
+	.radiusVievButton {
+		width: 80rpx;
+		height: 80rpx;
+		background-color: white;
+		border-radius: 50%;
+		border: 0.5px inside #e4e4e4;
+		box-shadow: 0px 1px 6px rgba(216, 216, 216, 50%);
+		display: grid; //生成3*3网格布局
+		grid-template-columns: 10% 80% 10%;
+		grid-template-rows: 10% 80% 10%;
+	}
+
+	#myPosition {
+		z-index: 1;
+		position: fixed;
+		top: 75vh;
+		right: 5vw;
+	}
+
+	#mainMenu {
+		z-index: 1;
+		position: fixed;
+		top: 75vh;
+		left: 5vw;
+	}
 </style>
--- Trucks_MotionTrajectory_Caretakers_项目代码/unpackage/dist/dev/mp-weixin/pages/index/index.js
+++ Trucks_MotionTrajectory_Caretakers_项目代码/unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -144,8 +144,10 @@ __webpack_require__.r(__webpack_exports__);
   !*** ./node_modules/babel-loader/lib!./node_modules/@dcloudio/vue-cli-plugin-uni/packages/webpack-preprocess-loader??ref--12-1!./node_modules/@dcloudio/webpack-uni-mp-loader/lib/script.js!./node_modules/@dcloudio/vue-cli-plugin-uni/packages/vue-loader/lib??vue-loader-options!./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js!D:/MyGit/github_my/Trucks_MotionTrajectory_Caretakers/Trucks_MotionTrajectory_Caretakers_项目代码/pages/index/index.vue?vue&type=script&lang=js& ***!
   \***********************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0; //
 //
 //
 //
@@ -157,8 +159,14 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
-//
+var _default =
+{
+  name: 'TrucksMap',
+  data: function data() {
+    return {};
+
+
+  } };exports.default = _default;
 
 /***/ }),
 /* 19 */
